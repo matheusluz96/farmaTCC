86940d21e8e70c6e7814925ffb9589f7ee2313c4
Rafaelblud <rafael-bella@hotmail.com>
2017-03-19 18:18:30 -0300

    Teste.java - Removendo comando a mais

--- src/farmaTCC/teste.java
+++ src/farmaTCC/teste.java
@@ -5,12 +5,7 @@ public class teste {
 	public static void main(String[] args){
 		conn.getConn();
 		conn.statusConn();
-		conn.closeConn();
-		conn.statusConn();
-		conn.getConn();
-		conn.statusConn();
-		conn.restartConn();
-		conn.statusConn();
+
 	}
 
 }
