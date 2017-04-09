18df70200d2a7b9b97dff1c939bd828ba2e3a2e6
matheusluz96 <matheusluz96@gmail.com>
2017-04-09 17:33:27 -0300

    Novas Janelas, Inicial/clientes

---
+++ src/farmaTCC/ClientesAtivos.java
@@ -0,0 +1,88 @@
+package farmaTCC;
+
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JList;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ClientesAtivos extends Main {
+
+	private JPanel contentPane;
+
+
+	public ClientesAtivos() {
+		
+		setTitle("Clientes");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{297, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JList list = new JList();
+		list.setBackground(Color.LIGHT_GRAY);
+		GridBagConstraints gbc_list = new GridBagConstraints();
+		gbc_list.gridheight = 4;
+		gbc_list.insets = new Insets(0, 0, 0, 5);
+		gbc_list.fill = GridBagConstraints.BOTH;
+		gbc_list.gridx = 0;
+		gbc_list.gridy = 1;
+		contentPane.add(list, gbc_list);
+		
+		JButton btnNewButton = new JButton("Verificar Cliente");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 1;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("Remover Cliente");
+		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_2.gridx = 1;
+		gbc_btnNewButton_2.gridy = 2;
+		contentPane.add(btnNewButton_2, gbc_btnNewButton_2);
+		
+		JButton btnNewButton_1 = new JButton("Adicionar Cliente");
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_1.gridx = 1;
+		gbc_btnNewButton_1.gridy = 3;
+		contentPane.add(btnNewButton_1, gbc_btnNewButton_1);
+		
+		JButton btnNewButton_4 = new JButton("Voltar");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmFarma.setVisible(true);
+				dispose();
+				
+				
+			}
+		});
+		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
+		gbc_btnNewButton_4.gridx = 1;
+		gbc_btnNewButton_4.gridy = 4;
+		contentPane.add(btnNewButton_4, gbc_btnNewButton_4);
+	}
+
+}
--- src/farmaTCC/Main.java
+++ src/farmaTCC/Main.java
@@ -1,5 +1,116 @@
 package farmaTCC;
 
-public class Main {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+import javax.swing.JButton;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class Main extends JFrame {
+
+	JFrame frmFarma;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frmFarma.setVisible(true);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	public void initialize() {
+		
+		frmFarma = new JFrame();
+		frmFarma.setResizable(false);
+		frmFarma.setTitle("Farma\r\n");
+		frmFarma.setBounds(100, 100, 600, 390);
+		frmFarma.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{100, 374, 98, 0};
+		gridBagLayout.rowHeights = new int[] {35, 35, 35, 35, 35, 35, 35, 35, 35, 35};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		frmFarma.getContentPane().setLayout(gridBagLayout);
+		
+		JButton btnEstoque = new JButton("Estoque");
+		btnEstoque.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnClientesInativos = new JButton("Clientes inativos");
+		btnClientesInativos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+
+		JButton btnClientesAtivos = new JButton("Clientes ativos");
+		btnClientesAtivos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ClientesAtivos frmAtivos = new ClientesAtivos();
+				frmAtivos.setVisible(true);
+				frmFarma.setVisible(false);
+				
+			}
+		});
+		GridBagConstraints gbc_btnClientesAtivos = new GridBagConstraints();
+		gbc_btnClientesAtivos.fill = GridBagConstraints.BOTH;
+		gbc_btnClientesAtivos.insets = new Insets(0, 0, 5, 0);
+		gbc_btnClientesAtivos.gridx = 2;
+		gbc_btnClientesAtivos.gridy = 5;
+		frmFarma.getContentPane().add(btnClientesAtivos, gbc_btnClientesAtivos);
+		GridBagConstraints gbc_btnClientesInativos = new GridBagConstraints();
+		gbc_btnClientesInativos.fill = GridBagConstraints.BOTH;
+		gbc_btnClientesInativos.insets = new Insets(0, 0, 5, 0);
+		gbc_btnClientesInativos.gridx = 2;
+		gbc_btnClientesInativos.gridy = 6;
+		frmFarma.getContentPane().add(btnClientesInativos, gbc_btnClientesInativos);
+		
+		JButton btnEncomendas = new JButton("Encomendas");
+		GridBagConstraints gbc_btnEncomendas = new GridBagConstraints();
+		gbc_btnEncomendas.fill = GridBagConstraints.BOTH;
+		gbc_btnEncomendas.insets = new Insets(0, 0, 5, 0);
+		gbc_btnEncomendas.gridx = 2;
+		gbc_btnEncomendas.gridy = 7;
+		frmFarma.getContentPane().add(btnEncomendas, gbc_btnEncomendas);
+		
+		JButton btnPromocao = new JButton("Promo\u00E7\u00F5es");
+		GridBagConstraints gbc_btnPromocao = new GridBagConstraints();
+		gbc_btnPromocao.fill = GridBagConstraints.BOTH;
+		gbc_btnPromocao.insets = new Insets(0, 0, 5, 0);
+		gbc_btnPromocao.gridx = 2;
+		gbc_btnPromocao.gridy = 8;
+		frmFarma.getContentPane().add(btnPromocao, gbc_btnPromocao);
+		GridBagConstraints gbc_btnEstoque = new GridBagConstraints();
+		gbc_btnEstoque.fill = GridBagConstraints.BOTH;
+		gbc_btnEstoque.gridx = 2;
+		gbc_btnEstoque.gridy = 9;
+		frmFarma.getContentPane().add(btnEstoque, gbc_btnEstoque);
+	}
+
 
 }
