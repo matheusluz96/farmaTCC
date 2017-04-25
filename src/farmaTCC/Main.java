bb99d6f4589f4cb456e9142316e4f61ec6856579
matheusluz96 <matheusluz96@gmail.com>
2017-04-25 13:35:09 -0300

    Correções Main

--- src/farmaTCC/Main.java
+++ src/farmaTCC/Main.java
@@ -2,14 +2,25 @@ package farmaTCC;
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Panel;
 
 
 public  class Main extends JFrame implements WindowListener {
@@ -53,7 +64,7 @@ public  class Main extends JFrame implements WindowListener {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{100, 374, 98, 0};
 		gridBagLayout.rowHeights = new int[] {35, 35, 35, 35, 35, 35, 35, 35, 35, 35};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 		frmFarma.getContentPane().setLayout(gridBagLayout);
 		
@@ -85,6 +96,28 @@ public  class Main extends JFrame implements WindowListener {
 				
 			}
 		});
+		
+		
+		
+		try {
+			Image img = ImageIO.read(new File("background.jpg"));
+			
+		} catch (IOException e1) {
+			
+			
+		}
+		
+		Panel panel = new Panel();
+		panel.setForeground(Color.DARK_GRAY);
+		panel.setBackground(Color.LIGHT_GRAY);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridheight = 9;
+		gbc_panel.gridwidth = 2;
+		gbc_panel.insets = new Insets(0, 0, 5, 5);
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 1;
+		frmFarma.getContentPane().add(panel, gbc_panel);
 		GridBagConstraints gbc_btnClientesAtivos = new GridBagConstraints();
 		gbc_btnClientesAtivos.fill = GridBagConstraints.BOTH;
 		gbc_btnClientesAtivos.insets = new Insets(0, 0, 5, 0);
@@ -97,6 +130,9 @@ public  class Main extends JFrame implements WindowListener {
 		gbc_btnClientesInativos.gridx = 2;
 		gbc_btnClientesInativos.gridy = 6;
 		frmFarma.getContentPane().add(btnClientesInativos, gbc_btnClientesInativos);
+		
+		
+
 		
 		JButton btnEncomendas = new JButton("Encomendas");
 		btnEncomendas.addActionListener(new ActionListener() {
